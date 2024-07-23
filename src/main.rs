306f969612c5ddaf545da1225d10a4713ec56c79
granta <grant.azure@sap.com>
2024-07-22 18:06:42 -0700

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Local};
 use rand;
 use rand::seq::SliceRandom;
 
@@ -14,53 +15,135 @@ fn letter_for_pool(pool: ExchangePool) -> char {
     letters.shuffle(&mut rng);
 
     match pool {
-        ExchangePool::IslandLife => 'I' ,
+        ExchangePool::IslandLife => 'I',
         ExchangePool::Grabergazureson => letters[0],
         ExchangePool::Pets => 'P',
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
 struct Participant {
     name: String,
     exchange_pools: Vec<ExchangePool>,
+    exclusions: Vec<String>,
 }
 
 impl Participant {
-    fn new(name: String, exchange_pools: Vec<ExchangePool>) -> Participant {
+    fn new(name: String, exchange_pools: Vec<ExchangePool>, exclusions: Vec<&str>) -> Participant {
+        let exclusions = exclusions.iter().map(|s| s.to_string()).collect();
         Participant {
             name,
             exchange_pools,
+            exclusions,
         }
     }
 }
 
+fn contains_exclusions(participants: &Vec<&Participant>) -> bool {
+    participants.iter().enumerate().any(|(i, p)| {
+        let receiver = &participants[(i + 1) % participants.len()].name;
+        p.exclusions.contains(&receiver)
+    })
+}
+
 fn main() {
-    let participants= vec![
-        Participant::new("Claire".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Grant".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Anne".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Duncan".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Noel".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("K-Lee".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Steve".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Linda".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Chris".to_string(), vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson]),
-        Participant::new("Jim".to_string(), vec![ExchangePool::Grabergazureson]),
-        Participant::new("Kari".to_string(), vec![ExchangePool::Grabergazureson]),
-        Participant::new("Meaghann".to_string(), vec![ExchangePool::Grabergazureson]),
-        Participant::new("Alec".to_string(), vec![ExchangePool::Grabergazureson]),
-        Participant::new("Eric".to_string(), vec![ExchangePool::IslandLife]),
-        Participant::new("Stella".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Bailey".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Kitty".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Charlie".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Astra".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Freya".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Lily".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Daisy".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Luca".to_string(), vec![ExchangePool::Pets]),
-        Participant::new("Kona".to_string(), vec![ExchangePool::Pets]),
+    let participants = vec![
+        Participant::new(
+            "Claire".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Duncan", "Meaghann"],
+        ),
+        Participant::new(
+            "Grant".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Chris"],
+        ),
+        Participant::new(
+            "Anne".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Eric", "K-Lee"],
+        ),
+        Participant::new(
+            "Duncan".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Claire", "Steve"],
+        ),
+        Participant::new(
+            "Noel".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["K-Lee", "Linda"],
+        ),
+        Participant::new(
+            "K-Lee".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Noel", "Kari"],
+        ),
+        Participant::new(
+            "Steve".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Linda", "Alec"],
+        ),
+        Participant::new(
+            "Linda".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Steve", "Anne"],
+        ),
+        Participant::new(
+            "Chris".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Jim"],
+        ),
+        Participant::new(
+            "Jim".to_string(),
+            vec![ExchangePool::Grabergazureson],
+            vec!["Kari", "Duncan"],
+        ),
+        Participant::new(
+            "Kari".to_string(),
+            vec![ExchangePool::Grabergazureson],
+            vec!["Jim", "Grant"],
+        ),
+        Participant::new(
+            "Meaghann".to_string(),
+            vec![ExchangePool::Grabergazureson],
+            vec!["Noel"],
+        ),
+        Participant::new(
+            "Alec".to_string(),
+            vec![ExchangePool::Grabergazureson],
+            vec!["Claire"],
+        ),
+        Participant::new(
+            "Eric".to_string(),
+            vec![ExchangePool::IslandLife, ExchangePool::Grabergazureson],
+            vec!["Anne"],
+        ),
+        Participant::new("Stella".to_string(), vec![ExchangePool::Pets], vec!["Lily"]),
+        Participant::new(
+            "Bailey".to_string(),
+            vec![ExchangePool::Pets],
+            vec!["Kitty"],
+        ),
+        Participant::new("Kitty".to_string(), vec![ExchangePool::Pets], vec!["Daisy"]),
+        Participant::new(
+            "Charlie".to_string(),
+            vec![ExchangePool::Pets],
+            vec!["Stella"],
+        ),
+        Participant::new(
+            "Astra".to_string(),
+            vec![ExchangePool::Pets],
+            vec!["Bailey"],
+        ),
+        Participant::new("Freya".to_string(), vec![ExchangePool::Pets], vec!["Astra"]),
+        Participant::new("Lily".to_string(), vec![ExchangePool::Pets], vec!["Freya"]),
+        Participant::new(
+            "Daisy".to_string(),
+            vec![ExchangePool::Pets],
+            vec!["Charlie"],
+        ),
+        Participant::new("Luca".to_string(), vec![ExchangePool::Pets], vec![]),
+        Participant::new("Kona".to_string(), vec![ExchangePool::Pets], vec![]),
     ];
     // get cli args
     let pool_arg = std::env::args().nth(1).expect("No pool specified");
@@ -69,19 +152,24 @@ fn main() {
         "graber" => ExchangePool::Grabergazureson,
         "pets" => ExchangePool::Pets,
         _ => panic!("Invalid pool specified"),
-     };
+    };
 
     let mut rng = rand::thread_rng();
-    let mut filtered_participants = participants.iter()
+    let mut filtered_participants = participants
+        .iter()
         .filter(|p| p.exchange_pools.contains(&pool))
         .collect::<Vec<&Participant>>();
 
+    // this is really dirty
     filtered_participants.shuffle(&mut rng);
-    filtered_participants.iter().enumerate()
-        .for_each(|(i, p)| {
-            let receiver = &filtered_participants[(i + 1) % filtered_participants.len()].name;
-            let sender = &p.name;
-            println!("{} -> {}", sender, receiver);
-        });
-    println!("Letter for 2023: {}", letter_for_pool(pool));
+    while contains_exclusions(&filtered_participants) {
+        filtered_participants.shuffle(&mut rng);
+    }
+    filtered_participants.iter().enumerate().for_each(|(i, p)| {
+        let receiver = &filtered_participants[(i + 1) % filtered_participants.len()].name;
+        let sender = &p.name;
+        println!("{} -> {}", sender, receiver);
+    });
+    let year = Local::now().year();
+    println!("Letter for {}: {}", year, letter_for_pool(pool));
 }
